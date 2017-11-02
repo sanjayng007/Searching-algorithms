e35b42274786bb244a8a12c086ab069eebd294d0
Sanjay Negi <sanjayng007@gmail.com>
2017-11-02 12:49:22 +0530

    Update Linear_Search.py

--- Linear_Search.py
+++ Linear_Search.py
@@ -1,6 +1,6 @@
 arr = [1,2,3,4,5]
 to_find = 5
-for i in arr:
-  if(i == to_find):
-    print("Found at index :" + str(arr.index(i)))
+for i in range(len(arr)):
+  if(arr[i] == to_find):
+    print("Found at index :" + str(i))
     break
