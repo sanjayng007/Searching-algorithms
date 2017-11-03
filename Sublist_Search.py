c1450832bc638708c2a1aa7cfc17ce8b9dad88f1
Sanjay Negi <sanjayng007@gmail.com>
2017-11-03 16:34:02 +0530

    Update Sublist_Search.py

--- Sublist_Search.py
+++ Sublist_Search.py
@@ -7,19 +7,19 @@ def sublist(to_search,to_find):
     j = 0
     while(j  <= small ):
       if(to_search[j+i] == to_find[j]):
-        j+=1 
-        if(j==small):
-          index = i
-          return index
+        j+=1
       else:
         i+=1 
         break
+      if(j==small+1):
+        index = i
+        return index
   return index
 
-list_to_find = [3,4,5,6]
-list_to_search=[1,2,3,4,5,6] 
+list_to_find = [3,4,5]
+list_from_find =[1,2,3,4,5,6] 
 
-start = sublist( list_to_search,list_to_find )
+start = sublist( list_from_find,list_to_find )
 
 if(start == None):
   print("not found")
